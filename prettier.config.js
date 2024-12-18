dc1e26ecbce7953a911a9eedfcbf066a92d15fb1
Hoon Choi <photiary@gmail.com>
2024-12-18 22:51:42 +0900

    prettier 자동 포맷 후 LF|CRLF 문자 차이로 commit 목록에 포함되지 않도록 endOfLine 속성 추가

--- prettier.config.js
+++ prettier.config.js
@@ -5,6 +5,7 @@
 const config = {
   singleQuote: true,
   trailingComma: 'es5',
+  endOfLine: 'auto', // LF|CRLF 자동으로 개행문자 사용 https://prettier.io/docs/en/options.html#end-of-line
 };
 
 export default config;
