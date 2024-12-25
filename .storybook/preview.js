520677c014cf75a47db132253eef8c62e4664d3b
Hoon Choi <photiary@gmail.com>
2024-12-25 19:19:00 +0900

    storybook 에서는 vue router가 불필요하여 제거

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,11 +2,10 @@ import { setup } from '@storybook/vue3';
 import { createPinia } from 'pinia';
 import '@/styles/style.scss';
 import '@/mocks';
-import router from '@/router';
 
 const pinia = createPinia();
 setup((app) => {
-  app.use(pinia).use(router);
+  app.use(pinia);
 });
 
 /** @type { import('@storybook/vue3').Preview } */
